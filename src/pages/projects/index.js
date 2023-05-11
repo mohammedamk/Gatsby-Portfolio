8f67d789af4146c58a4409ff7cf5713e13d0e667
mohammed <mohammedamk1@gmail.com>
2023-05-11 10:40:58 +0500

    making responsive for mobile device

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,8 +1,8 @@
 import { Link, graphql } from "gatsby";
 import React from "react";
 import Layout from "../../components/layout";
-import "../../styles/projects.module.css"
 import Img from "gatsby-image"
+import "../../styles/projects.module.css"
 
 export default function Projects( {data} ) {
   console.log("data",data);
@@ -18,14 +18,13 @@ export default function Projects( {data} ) {
       <div className="portfolio">
         <h3>Project File</h3>
         <p>Project Portfolio , I will make this project</p>
-      </div>
 
       <div className="staticData">
         {projectData.map(projects => (
 
           <Link to={"/projects/" + projects.frontmatter.slug} key={projects.id}>
             <div className="portfoliosData">
-                <Img fluid={projects.frontmatter.thumb.childImageSharp.fluid} />
+                <Img fluid={projects.frontmatter.thumb.childImageSharp.fluid} className="thumbImage" />
               <h3>{projects.frontmatter.title}</h3>
               <p> {projects.frontmatter.stack} </p>
             </div>
@@ -35,13 +34,10 @@ export default function Projects( {data} ) {
        <div className="contact">
         <p> Like what you see? Email me at {contact} for a quote! </p>
       </div>
-
+</div>
     </Layout>
   )
 }
-
-
-
 
 
 
